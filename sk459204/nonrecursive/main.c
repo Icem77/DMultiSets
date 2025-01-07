d7726a62fb6154e472d900e57181065f2bd541cc
Szymon Kruk <sk459204@students.mimuw.edu.pl>
2025-01-07 08:44:51 +0100

    Leak free SumsetPool (~1.25 reference time)

--- sk459204/nonrecursive/main.c
+++ sk459204/nonrecursive/main.c
@@ -10,6 +10,8 @@ typedef struct SmartSumset {
     Sumset sumset;
     struct SmartSumset* parent;
     int reference_count;
+
+    struct SmartSumset* next_on_free_list; // connection in memory pool
 } SmartSumset_t;
 
 typedef struct PairToSolve {
@@ -18,28 +20,82 @@ typedef struct PairToSolve {
 } PairToSolve_t;
 
 typedef struct Stack {
-    PairToSolve_t** stack;
+    PairToSolve_t* stack;
     int last_push_index;
     int stack_size;
 } Stack_t;
 
+typedef struct SmartSumsetPool {
+    SmartSumset_t* pool;
+    SmartSumset_t* free_list;
+    int pool_size;
+} SmartSumsetPool_t;
+
+SmartSumsetPool_t* pool_init(int pool_size) {
+    SmartSumsetPool_t* pool = (SmartSumsetPool_t*) malloc(sizeof(SmartSumsetPool_t));
+    pool->pool = (SmartSumset_t*) malloc(pool_size * sizeof(SmartSumset_t));
+    pool->free_list = &pool->pool[0];
+    pool->pool_size = pool_size;
+
+    for (int i = 0; i < pool_size - 1; ++i) {
+        pool->pool[i].next_on_free_list = &pool->pool[i + 1];
+    }
+    pool->pool[pool_size - 1].next_on_free_list = NULL;
+
+    return pool;
+}
+
+SmartSumset_t* pool_get(SmartSumsetPool_t* pool) {
+    if (pool->free_list == NULL) {
+        pool->pool = (SmartSumset_t*) realloc(pool->pool, 2 * pool->pool_size * sizeof(SmartSumset_t));
+        pool->pool_size *= 2;
+        pool->free_list = &pool->pool[pool->pool_size / 2];
+
+        for (int i = pool->pool_size / 2; i < pool->pool_size - 1; ++i) {
+            pool->pool[i].next_on_free_list = &pool->pool[i + 1];
+        }
+        pool->pool[pool->pool_size - 1].next_on_free_list = NULL;
+    }
+
+    SmartSumset_t* result = pool->free_list;
+    pool->free_list = result->next_on_free_list;
+
+    return result;
+}
+
+void pool_return(SmartSumsetPool_t* pool, SmartSumset_t* smart_sumset) {
+    smart_sumset->next_on_free_list = pool->free_list;
+    pool->free_list = smart_sumset;
+}
+
+void pool_destroy(SmartSumsetPool_t* pool) {
+    free(pool->pool);
+    free(pool);
+}
+
 Stack_t* stack_init(int stack_size) {
     Stack_t* stack = (Stack_t*) malloc(sizeof(Stack_t));
-    stack->stack = (PairToSolve_t**) malloc(stack_size * sizeof(PairToSolve_t*));
+    stack->stack = (PairToSolve_t*) malloc(stack_size * sizeof(PairToSolve_t));
     stack->last_push_index = -1;
     stack->stack_size = stack_size;
     return stack;
 }
 
-void stack_push(Stack_t* stack, PairToSolve_t* pair) {
-    stack->stack[++stack->last_push_index] = pair;
+void stack_push(Stack_t* stack, SmartSumset_t* a, SmartSumset_t* b) {
+    stack->last_push_index++;
+    if (stack->last_push_index == stack->stack_size) {
+        stack->stack = (PairToSolve_t*) realloc(stack->stack, 2 * stack->stack_size * sizeof(PairToSolve_t));
+        stack->stack_size *= 2;
+    }
+
+    stack->stack[stack->last_push_index].a = a;
+    stack->stack[stack->last_push_index].b = b;
 }
 
-PairToSolve_t* stack_pop(Stack_t* stack) {
-    PairToSolve_t* result = stack->stack[stack->last_push_index];
-    stack->stack[stack->last_push_index--] = NULL;
-
-    return result;
+void stack_pop(Stack_t* stack, SmartSumset_t** a, SmartSumset_t** b) {
+    *a = stack->stack[stack->last_push_index].a;
+    *b = stack->stack[stack->last_push_index].b;
+    stack->last_push_index--;
 }
 
 bool stack_is_empty(Stack_t* stack) {
@@ -51,23 +107,17 @@ void stack_destroy(Stack_t* stack) {
     free(stack);
 }
 
-void sumset_swap(Sumset** a, Sumset** b) {
-    Sumset* tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
 void smart_sumset_swap(SmartSumset_t** a, SmartSumset_t** b) {
     SmartSumset_t* tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-void check_sumset_reference_count(SmartSumset_t* sumset) {
+void check_sumset_reference_count(SmartSumsetPool_t* pool, SmartSumset_t* sumset) {
     while (sumset != NULL && sumset->reference_count == 0) {
         SmartSumset_t* tmp = sumset;
         sumset = sumset->parent;
-        free(tmp);
+        pool_return(pool, tmp);
 
         if (sumset != NULL) {
             sumset->reference_count--;
@@ -75,29 +125,24 @@ void check_sumset_reference_count(SmartSumset_t* sumset) {
     }
 }
 
-void nonrecursive_dummy_solv_no_leaks(InputData* input_data, Solution* best_solution) {
-    Stack_t* stack = stack_init(8192);
+void nonrecursive_pool_solv(InputData* input_data, Solution* best_solution) {
+    SmartSumsetPool_t* pool = pool_init(16384);
 
-    SmartSumset_t* a = (SmartSumset_t*) malloc(sizeof(SmartSumset_t));
+    SmartSumset_t* a = pool_get(pool);
     a->sumset = input_data->a_start;
     a->parent = NULL;
     a->reference_count = 2;
 
-    SmartSumset_t* b = (SmartSumset_t*) malloc(sizeof(SmartSumset_t));
+    SmartSumset_t* b = pool_get(pool);
     b->sumset = input_data->b_start;
     b->parent = NULL;
     b->reference_count = 2;
 
-    PairToSolve_t* pairToPush = (PairToSolve_t*) malloc(sizeof(PairToSolve_t));
-    pairToPush->a = a; 
-    pairToPush->b = b;
-
-    stack_push(stack, pairToPush);
+    Stack_t* stack = stack_init(8192);
+    stack_push(stack, a, b);
 
     while (!stack_is_empty(stack)) {
-        PairToSolve_t* pair = stack_pop(stack);
-        SmartSumset_t* a = pair->a; 
-        SmartSumset_t* b = pair->b;
+        stack_pop(stack, &a, &b);
 
         if (a->sumset.sum > b->sumset.sum) {
             smart_sumset_swap(&a, &b);
@@ -106,19 +151,15 @@ void nonrecursive_dummy_solv_no_leaks(InputData* input_data, Solution* best_solu
         if (is_sumset_intersection_trivial(&a->sumset, &b->sumset)) { // s(a) ∩ s(b) = {0}.
             for (size_t i = a->sumset.last; i <= input_data->d; ++i) {
                 if (!does_sumset_contain(&b->sumset, i)) {
-                    SmartSumset_t* a_with_i = (SmartSumset_t*) malloc(sizeof(SmartSumset_t));
+                    SmartSumset_t* a_with_i = pool_get(pool);
                     a_with_i->reference_count = 1;
                     a_with_i->parent = a;
                     sumset_add(&a_with_i->sumset, &a->sumset, i);
 
-                    PairToSolve_t* new_pair = (PairToSolve_t*) malloc(sizeof(PairToSolve_t));
-                    new_pair->a = a_with_i;
-                    new_pair->b = b;
-
                     a->reference_count++;
                     b->reference_count++;
 
-                    stack_push(stack, new_pair);
+                    stack_push(stack, a_with_i, b);
                 }
             }
         } else if ((a->sumset.sum == b->sumset.sum) && (get_sumset_intersection_size(&a->sumset, &b->sumset) == 2)) { // s(a) ∩ s(b) = {0, ∑b}.
@@ -129,28 +170,25 @@ void nonrecursive_dummy_solv_no_leaks(InputData* input_data, Solution* best_solu
 
         a->reference_count--;
         b->reference_count--;
-        
-        check_sumset_reference_count(a);
-        check_sumset_reference_count(b);
 
-        free(pair);
+        check_sumset_reference_count(pool, a);
+        check_sumset_reference_count(pool, b);
     }
 
     stack_destroy(stack);
-    free(a);
-    free(b);
+    pool_destroy(pool);
 }
 
 int main()
 {
     InputData input_data;
     //input_data_read(&input_data);
-    input_data_init(&input_data, 8, 10, (int[]){0}, (int[]){1, 0});
+    input_data_init(&input_data, 8, 34, (int[]){0}, (int[]){1, 0});
 
     Solution best_solution;
     solution_init(&best_solution);
 
-    nonrecursive_dummy_solv_no_leaks(&input_data, &best_solution);
+    nonrecursive_pool_solv(&input_data, &best_solution);
 
     solution_print(&best_solution);
     return 0;
