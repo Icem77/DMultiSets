9c95a38b036ab96f07c8dddd2b15790d45767f5a
Szymon Kruk <sk459204@students.mimuw.edu.pl>
2025-01-11 11:01:59 +0100

    First take on recursive parallel - not working at all

--- sk459204/parallel/main.c
+++ sk459204/parallel/main.c
@@ -9,10 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define GIVE_AWAY_FREQUENCY 15
-#define ITERATIONS_TO_GIVEAWAY 64
 #define INITIAL_POOL_SIZE 8192
-#define CHECK_AFTER 32
 
 typedef struct SmartParallelSumset {
     Sumset sumset;
@@ -26,14 +23,13 @@ typedef struct SmartParallelSumsetPool {
     SmartParallelSumset_t* pool;
     SmartParallelSumset_t* free_list;
     int pool_size;
-    int free_list_size;
 
     pthread_mutex_t mutex;
 } SmartParallelSumsetPool_t;
 
 typedef struct SharedStack {
     SmartParallelSumset_t** stack;
-    int last_push_index; // do not use atomic cause we have mutex
+    atomic_int last_push_index; // do not use atomic cause we have mutex
     int stack_size;
 
     pthread_mutex_t mutex;
@@ -52,7 +48,7 @@ typedef struct ThreadStarterPack {
 SharedStack_t* shared_stack_init(int stack_size) {
     SharedStack_t* stack = (SharedStack_t*) malloc(sizeof(SharedStack_t));
     stack->stack = (SmartParallelSumset_t**) malloc(stack_size * sizeof(SmartParallelSumset_t*));
-    stack->last_push_index = -1;
+    atomic_store(&stack->last_push_index, -1);
     stack->stack_size = stack_size;
     stack->threads_waiting = 0;
     stack->calculations_finished = false;
@@ -69,11 +65,12 @@ void shared_stack_destroy(SharedStack_t* stack) {
     free(stack);
 }
 
-void give_away_branch(SharedStack_t* stack, SmartParallelSumset_t* a, SmartParallelSumset_t* b) {
+void give_away_branches(SharedStack_t* stack, SmartParallelSumset_t** sumsets, int counter) {
     pthread_mutex_lock(&stack->mutex);
-    stack->last_push_index += 2;
-    stack->stack[stack->last_push_index - 1] = a;
-    stack->stack[stack->last_push_index] = b;
+    for (int i = 1; i <= counter; ++i) {
+        stack->stack[stack->last_push_index + i] = sumsets[i];
+    }
+    atomic_fetch_add(&stack->last_push_index, counter);
     pthread_mutex_unlock(&stack->mutex);
     pthread_cond_signal(&stack->waiting_room);
 }
@@ -104,7 +101,7 @@ void take_new_branch(SharedStack_t* stack, SmartParallelSumset_t** a, SmartParal
 
     *b = stack->stack[stack->last_push_index];
     *a = stack->stack[stack->last_push_index - 1];
-    stack->last_push_index -= 2;
+    atomic_fetch_sub(&stack->last_push_index, 2);
 
     pthread_mutex_unlock(&stack->mutex);
 }
@@ -114,7 +111,6 @@ SmartParallelSumsetPool_t* pool_init(int pool_size) {
     pool->pool = (SmartParallelSumset_t*) malloc(pool_size * sizeof(SmartParallelSumset_t));
     pool->free_list = &pool->pool[0];
     pool->pool_size = pool_size;
-    pool->free_list_size = pool_size;
 
     for (int i = 0; i < pool_size - 1; ++i) {
         pool->pool[i].next_on_free_list = &pool->pool[i + 1];
@@ -132,13 +128,12 @@ void pool_destroy(SmartParallelSumsetPool_t* pool) {
     free(pool);
 }
 
-SmartParallelSumset_t* pool_get(SmartParallelSumsetPool_t* pool) {
+SmartParallelSumset_t* pool_get(SmartParallelSumsetPool_t* pool, int how_much) {
     pthread_mutex_lock(&pool->mutex);
     if (pool->free_list == NULL) {
         pool->pool = (SmartParallelSumset_t*) realloc(pool->pool, 2 * pool->pool_size * sizeof(SmartParallelSumset_t));
         pool->pool_size *= 2;
         pool->free_list = &pool->pool[pool->pool_size / 2];
-        pool->free_list_size = pool->pool_size / 2;
 
         for (int i = pool->pool_size / 2; i < pool->pool_size - 1; ++i) {
             pool->pool[i].next_on_free_list = &pool->pool[i + 1];
@@ -146,177 +141,151 @@ SmartParallelSumset_t* pool_get(SmartParallelSumsetPool_t* pool) {
         pool->pool[pool->pool_size - 1].next_on_free_list = NULL;
     }
 
+    int i = 1;
     SmartParallelSumset_t* result = pool->free_list;
-    pool->free_list = result->next_on_free_list;
-    pool->free_list_size -= 1;
+    SmartParallelSumset_t* tail = result;
+    while (i < how_much) {
+        tail = tail->next_on_free_list;
+        i++;
+    }
+    pool->free_list = tail->next_on_free_list;
+    tail->next_on_free_list = NULL;
     pthread_mutex_unlock(&pool->mutex);
 
     return result;
 }
 
-void pool_return(SmartParallelSumsetPool_t* pool, SmartParallelSumset_t* smart_sumset) {
+void pool_return(SmartParallelSumsetPool_t* pool, SmartParallelSumset_t* returning) {
     pthread_mutex_lock(&pool->mutex);
-    pool->free_list_size += 1;
-    smart_sumset->next_on_free_list = pool->free_list;
-    pool->free_list = smart_sumset;
+    returning->next_on_free_list = pool->free_list;
+    pool->free_list = returning;
     pthread_mutex_unlock(&pool->mutex);
 }
 
-int free_list_size(SmartParallelSumsetPool_t* pool) {
-    return pool->free_list_size;
-}
-
-typedef struct Stack {
-    SmartParallelSumset_t** stack;
-    int last_push_index;
-    int stack_size;
-} Stack_t;
-
-Stack_t* stack_init(int stack_size) {
-    Stack_t* stack = (Stack_t*) malloc(sizeof(Stack_t));
-    stack->stack = (SmartParallelSumset_t**) malloc(stack_size * sizeof(SmartParallelSumset_t*));
-    stack->last_push_index = -1;
-    stack->stack_size = stack_size;
-    return stack;
-}
-
-void stack_push(Stack_t* stack, SmartParallelSumset_t* a, SmartParallelSumset_t* b) {
-    if (stack->last_push_index == stack->stack_size - 1) {
-        stack->stack = (SmartParallelSumset_t**) realloc(stack->stack, 2 * stack->stack_size * sizeof(SmartParallelSumset_t*));
-        stack->stack_size *= 2;
-    }
-
-    stack->last_push_index += 2;
-    stack->stack[stack->last_push_index - 1] = a;
-    stack->stack[stack->last_push_index] = b;
-}
-
-void stack_pop(Stack_t* stack, SmartParallelSumset_t** a, SmartParallelSumset_t** b) {
-    *b = stack->stack[stack->last_push_index];
-    *a = stack->stack[stack->last_push_index - 1];
-    stack->last_push_index -= 2;
-}
-
-bool stack_is_empty(Stack_t* stack) {
-    return stack->last_push_index == -1;
-}
-
-void stack_destroy(Stack_t* stack) {
-    free(stack->stack);
-    free(stack);
-}
-
-void smart_sumset_swap(SmartParallelSumset_t** a, SmartParallelSumset_t** b) {
+void swap(SmartParallelSumset_t** a, SmartParallelSumset_t** b) {
     SmartParallelSumset_t* tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-void check_sumset_reference_count(SmartParallelSumsetPool_t* pool, SmartParallelSumset_t* sumset) {
-    while (atomic_fetch_sub(&sumset->reference_count, 1) == 1) {
-        SmartParallelSumset_t* tmp = sumset;
-        sumset = sumset->parent;
-        pool_return(pool, tmp);
-        if (sumset == NULL) break;
+void branch_break(ThreadStarterPack_t* resources, SmartParallelSumset_t* a, SmartParallelSumset_t* b) {
+    if (a->sumset.sum > b->sumset.sum) swap(&a, &b);
+
+    if (is_sumset_intersection_trivial(&a->sumset, &b->sumset)) { // s(a) ∩ s(b) = {0}.
+        int counter = 0;
+        int expansions[resources->input_data->d - a->sumset.last + 1];
+        for (size_t i = a->sumset.last; i <= resources->input_data->d; ++i) {
+            if (!does_sumset_contain(&b->sumset, i)) {
+                expansions[counter] = i;
+                counter++;
+            }
+        }
+
+        SmartParallelSumset_t* sumset = pool_get(resources->common_pool, counter);
+        SmartParallelSumset_t* expandedSumsets[2*counter];
+        for (int i = 0; i < counter; i += 2) {
+            sumset_add(&sumset->sumset, &a->sumset, expansions[i]);
+            atomic_store(&sumset->reference_count, 0);
+            expandedSumsets[i] = sumset;
+            expandedSumsets[i + 1] = b;
+            sumset = sumset->next_on_free_list;
+        }
+
+        atomic_fetch_add(&a->reference_count, counter);
+        atomic_fetch_add(&b->reference_count, counter);
+        give_away_branches(resources->common_stack, expandedSumsets, 2*counter);
+
+    } else if ((a->sumset.sum == b->sumset.sum) && (get_sumset_intersection_size(&a->sumset, &b->sumset) == 2)) { // s(a) ∩ s(b) = {0, ∑b}.
+        if (a->sumset.sum > resources->my_best_solution->sum) {
+            solution_build(resources->my_best_solution, resources->input_data, &a->sumset, &b->sumset);
+        }
     }
 }
 
-void* thread_calculation(void* args) {
-    ThreadStarterPack_t* starter_pack = (ThreadStarterPack_t*) args;
-    Stack_t* my_stack = stack_init(125000);
+void recursive_branch_solve(ThreadStarterPack_t* resources, SmartParallelSumset_t* a, SmartParallelSumset_t* b) {
+    if (a->sumset.sum > b->sumset.sum) recursive_branch_solve(resources, b, a);
 
-    SmartParallelSumset_t* expansions[starter_pack->input_data->d];
-    int index = 0;
+    if (is_sumset_intersection_trivial(&a->sumset, &b->sumset)) { // s(a) ∩ s(b) = {0}.
+        for (size_t i = a->sumset.last; i <= resources->input_data->d; ++i) {
+            if (!does_sumset_contain(&b->sumset, i)) {
+                SmartParallelSumset_t a_with_i;
+                sumset_add(&a_with_i.sumset, &a->sumset, i);
+                a_with_i.parent = a;
+                atomic_store(&a_with_i.reference_count, 0);
+                recursive_branch_solve(resources, &a_with_i, b);
+            }
+        }
+    } else if ((a->sumset.sum == b->sumset.sum) && (get_sumset_intersection_size(&a->sumset, &b->sumset) == 2)) { // s(a) ∩ s(b) = {0, ∑b}.
+        if (b->sumset.sum > resources->my_best_solution->sum)
+            solution_build(resources->my_best_solution, resources->input_data, &a->sumset, &b->sumset);
+    }
+}
 
-    SmartParallelSumset_t* a = NULL;
-    SmartParallelSumset_t* b = NULL;
-    take_new_branch(starter_pack->common_stack, &a, &b, starter_pack->input_data->t);
-    int check = 0;
+void clean_up(SmartParallelSumsetPool_t* pool, SmartParallelSumset_t* check) {
+    if (atomic_fetch_sub(&check->reference_count, 1) == 1) {
+        SmartParallelSumset_t* toCheck = check->parent;
+        pool_return(pool, check);
+        clean_up(pool, toCheck);
+    }
+}
 
+void* thread_calculations(void* args) {
+    ThreadStarterPack_t* resources = (ThreadStarterPack_t*) args;
+    SmartParallelSumset_t* a;
+    SmartParallelSumset_t* b;
+
+    take_new_branch(resources->common_stack, &a, &b, resources->input_data->t);
+    
     while (a != NULL && b != NULL) {
-        stack_push(my_stack, a, b);
-
-        while (!stack_is_empty(my_stack)) {
-            stack_pop(my_stack, &a, &b);
-
-            if (a->sumset.sum > b->sumset.sum) {
-                smart_sumset_swap(&a, &b);
-            }
-
-            if (is_sumset_intersection_trivial(&a->sumset, &b->sumset)) { // s(a) ∩ s(b) = {0}.
-                index = 0;
-                for (size_t i = a->sumset.last; i <= starter_pack->input_data->d; ++i) {
-                    if (!does_sumset_contain(&b->sumset, i)) {
-                        SmartParallelSumset_t* a_with_i = pool_get(starter_pack->common_pool);
-                        atomic_store(&a_with_i->reference_count, 1);
-                        a_with_i->parent = a;
-                        sumset_add(&a_with_i->sumset, &a->sumset, i);
-                        
-                        expansions[index] = a_with_i;
-                        index++;
-                    }
-                }
-
-                atomic_fetch_add(&a->reference_count, index);
-                atomic_fetch_add(&b->reference_count, index);
-
-                if (check == CHECK_AFTER && starter_pack->common_stack->last_push_index < starter_pack->input_data->t) {
-                    check = 0;
-                    for (int i = 0; i < index; ++i) {
-                        give_away_branch(starter_pack->common_stack, expansions[i], b);
-                    }
-                } else {
-                    for (int i = 0; i < index; ++i) {
-                        stack_push(my_stack, expansions[i], b);  
-                    }
-                }
-            } else if ((a->sumset.sum == b->sumset.sum) && (get_sumset_intersection_size(&a->sumset, &b->sumset) == 2)) { // s(a) ∩ s(b) = {0, ∑b}.
-                if (a->sumset.sum > starter_pack->my_best_solution->sum) {
-                    solution_build(starter_pack->my_best_solution, starter_pack->input_data, &a->sumset, &b->sumset);
-                }
-            }
-
-            check_sumset_reference_count(starter_pack->common_pool, a);
-            check_sumset_reference_count(starter_pack->common_pool, b);
+        if (atomic_load(&resources->common_stack->last_push_index) + 1 < 2 * resources->input_data->t) {
+            branch_break(resources, a, b);
+        } else {
+            recursive_branch_solve(resources, a, b);
+            if (a->parent != NULL) clean_up(resources->common_pool, a->parent);
+            if (b->parent != NULL) clean_up(resources->common_pool, b->parent);
         }
 
-        take_new_branch(starter_pack->common_stack, &a, &b, starter_pack->input_data->t);
+        take_new_branch(resources->common_stack, &a, &b, resources->input_data->t);
     }
 
-    stack_destroy(my_stack);
-    
     return NULL;
 }
 
 int main()
 {   
     /*
-    ZAŁOŻENIE PIERWOTNEJ WERSJI PROGRAMU:
-    - program wykonuje poprawnie obliczenia
-    - program jest pozbawiony wyciekow pamieci
-    - program jest pozbawiony bledow synchronizacyjnych
-    - program wykorzystuje naiwna taktyke oddawania i pobierania zadan ze stosu
-    - program wykorzystuje naiwna taktyke oddawania i pobierania sumsetow z puli
+    ZAŁOŻENIE REKURENCYJNEJ WERSJI PROGRAMU:
+    - wez branch ze wspolnej puli pracy
+    - -> jezeli na stosie jest wystarczajacy zapas zlecen:
+            - rekurencyjnie rozwiaz branch
+            - sprawdz czy nalezy zwolnic fundament na ktorym byl oparty branch
+            - podczas rekurencji zbierz liste Sumsetow do uwolnienia i oddaj ja do puli
+    - -> wpp:
+            - wez z puli sumsety i rozbij branch 
+            - oddaj rozbite branche na stos
     */
 
     InputData input_data;
     //input_data_read(&input_data);
-    input_data_init(&input_data, 1, 28, (int[]){0}, (int[]){1, 0});
+    input_data_init(&input_data, 1, 4, (int[]){0}, (int[]){1, 0});
 
-    SharedStack_t* common_stack = shared_stack_init(125000);
+    SharedStack_t* common_stack = shared_stack_init(5000);
     SmartParallelSumsetPool_t* common_pool = pool_init(input_data.t * INITIAL_POOL_SIZE);
 
-    SmartParallelSumset_t* a = pool_get(common_pool);
+    SmartParallelSumset_t* a = pool_get(common_pool, 1); //pool_get(common_pool);
     a->sumset = input_data.a_start;
     a->parent = NULL;
     atomic_store(&a->reference_count, 10);
 
-    SmartParallelSumset_t* b = pool_get(common_pool);
+    SmartParallelSumset_t* b = pool_get(common_pool, 1);
     b->sumset = input_data.b_start;
     b->parent = NULL;
     atomic_store(&b->reference_count, 10);
 
-    give_away_branch(common_stack, a, b);
+    SmartParallelSumset_t* start[2];
+    start[0] = a;
+    start[1] = b;
+    give_away_branches(common_stack, start, 2);
     
     pthread_t threads[input_data.t];
     ThreadStarterPack_t* starter_packs = (ThreadStarterPack_t*) malloc(input_data.t * sizeof(ThreadStarterPack_t)); 
@@ -327,7 +296,7 @@ int main()
         starter_packs[i].common_stack = common_stack;
         starter_packs[i].common_pool = common_pool;
         starter_packs[i].input_data = &input_data;
-        pthread_create(&threads[i], NULL, thread_calculation, (void*)&starter_packs[i]);
+        pthread_create(&threads[i], NULL, thread_calculations, (void*)&starter_packs[i]);
     }
 
     // wait for the end of calculations
