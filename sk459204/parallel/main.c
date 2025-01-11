1dbc75aeead32210d2446044dbb234fe38fa827e
Szymon Kruk <sk459204@students.mimuw.edu.pl>
2025-01-11 18:31:01 +0100

    Added SPSPool to parallel, raport generating script

--- sk459204/parallel/main.c
+++ sk459204/parallel/main.c
@@ -10,15 +10,27 @@
 #include <stdio.h>
 
 #define INITIAL_BRANCH_POOL_SIZE 8192
+#define INITIAL_SUMSET_POOL_SIZE 1024
 
 atomic_int locks = 0;
 atomic_int frees = 0;
+int max_sps_use = 0;
+
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
 
 typedef struct SmartParallelSumset {
     Sumset sumset;
     struct SmartParallelSumset* parent;
 
     atomic_int parent_to;
+
+    struct SmartParallelSumset* next_on_free_list;
 } SPS_t;
 
 typedef struct BranchPool {
@@ -34,11 +46,58 @@ typedef struct BranchPool {
     bool finish;
 } BranchPool_t;
 
+typedef struct SPSPool {
+    SPS_t* pool;
+    SPS_t* free_list;
+    int pool_size;
+
+    pthread_mutex_t mutex;
+} SPSPool_t;
+
 typedef struct ThreadResources {
     BranchPool_t* branch_pool;
     InputData* input;
     Solution* mySolution;
+    SPSPool_t* sps_pool;
 } TR_t;
+
+SPSPool_t* sps_pool_init() {
+    SPSPool_t* sps_pool = (SPSPool_t*) malloc(sizeof(SPSPool_t));
+    sps_pool->pool = (SPS_t*) malloc(INITIAL_SUMSET_POOL_SIZE * sizeof(SPS_t));
+    sps_pool->pool_size = INITIAL_SUMSET_POOL_SIZE;
+
+    for (int i = 0; i < sps_pool->pool_size - 1; ++i) {
+        sps_pool->pool[i].next_on_free_list = &sps_pool->pool[i + 1];
+    }
+    sps_pool->pool[sps_pool->pool_size - 1].next_on_free_list = NULL;
+
+    sps_pool->free_list = &sps_pool->pool[0];
+
+    pthread_mutex_init(&sps_pool->mutex, NULL);
+
+    return sps_pool;
+}
+
+SPS_t* sps_pool_get(SPSPool_t* pool) {
+    pthread_mutex_lock(&pool->mutex);
+    SPS_t* to_return = pool->free_list;
+    pool->free_list = pool->free_list->next_on_free_list;
+    pthread_mutex_unlock(&pool->mutex);
+    return to_return;
+}
+
+void sps_pool_return(SPSPool_t* pool, SPS_t* returning) {
+    pthread_mutex_lock(&pool->mutex);
+    returning->next_on_free_list = pool->free_list;
+    pool->free_list = returning;
+    pthread_mutex_unlock(&pool->mutex);
+}
+
+void sps_pool_destroy(SPSPool_t* pool) {
+    free(pool->pool);
+    pthread_mutex_destroy(&pool->mutex);
+    free(pool);
+}
 
 BranchPool_t* branch_pool_init() {
     BranchPool_t* pool = (BranchPool_t*) malloc(sizeof(BranchPool_t));
@@ -114,12 +173,12 @@ void swap(SPS_t** a, SPS_t** b) {
     *b = tmp;
 }
 
-void check_if_free(SPS_t* a) {
+void check_if_free(SPSPool_t* pool, SPS_t* a) {
     if (atomic_fetch_sub(&a->parent_to, 1) == 1) {
         SPS_t* parent_to_check = a->parent;
         atomic_fetch_add(&frees, 1);
-        free(a);
-        check_if_free(parent_to_check);
+        sps_pool_return(pool, a);
+        check_if_free(pool, parent_to_check);
     }    
 }
 
@@ -132,7 +191,8 @@ void branch_split(TR_t* resources, SPS_t* a, SPS_t* b) {
         for (size_t i = a->sumset.last; i <= resources->input->d; ++i) {
             if (!does_sumset_contain(&b->sumset, i)) {
                 atomic_fetch_add(&locks, 1);
-                SPS_t* a_with_i = (SPS_t*) malloc(sizeof(SPS_t));
+                max_sps_use = max(max_sps_use, atomic_load(&locks) - atomic_load(&frees));
+                SPS_t* a_with_i = sps_pool_get(resources->sps_pool);
                 a_with_i->parent = a;
                 atomic_store(&a_with_i->parent_to, 1);
 
@@ -143,18 +203,14 @@ void branch_split(TR_t* resources, SPS_t* a, SPS_t* b) {
                 give_away_branch(resources->branch_pool, a_with_i, b);
             }
         }
-        // zliczamy sie w lisciach 
-        check_if_free(a);
-        check_if_free(b);
-    } else {
-        if ((a->sumset.sum == b->sumset.sum) && (get_sumset_intersection_size(&a->sumset, &b->sumset) == 2)) { // s(a) ∩ s(b) = {0, ∑b}.
-            if (a->sumset.sum > resources->mySolution->sum) {
-                solution_build(resources->mySolution, resources->input, &a->sumset, &b->sumset);
-            }
+    } else if ((a->sumset.sum == b->sumset.sum) && (get_sumset_intersection_size(&a->sumset, &b->sumset) == 2)) { // s(a) ∩ s(b) = {0, ∑b}.
+        if (a->sumset.sum > resources->mySolution->sum) {
+            solution_build(resources->mySolution, resources->input, &a->sumset, &b->sumset);
         }
-        check_if_free(a);
-        check_if_free(b);
-    } 
+    }
+
+    check_if_free(resources->sps_pool, a);
+    check_if_free(resources->sps_pool, b);
 }
 
 void recursive_solv(TR_t* resources, SPS_t* a, SPS_t* b) {
@@ -190,8 +246,8 @@ void* thread_calculations(void* args) {
             branch_split(resources, a, b);
         } else {
             recursive_solv(resources, a, b);
-            check_if_free(a);
-            check_if_free(b);
+            check_if_free(resources->sps_pool, a);
+            check_if_free(resources->sps_pool, b);
         }
 
         take_new_branch(resources->branch_pool, &a, &b, resources->input->t);
@@ -204,9 +260,10 @@ int main()
 {   
     InputData input_data;
     //input_data_read(&input_data);
-    input_data_init(&input_data, 8, 34, (int[]){0}, (int[]){1, 0});
+    input_data_init(&input_data, 12, 28, (int[]){0}, (int[]){1, 0});
 
     BranchPool_t* common_branch_pool = branch_pool_init();
+    SPSPool_t* sps_pool = sps_pool_init();
 
     SPS_t a;
     a.sumset = input_data.a_start;
@@ -228,6 +285,7 @@ int main()
         starterPacks[i].branch_pool = common_branch_pool;
         starterPacks[i].input = &input_data;
         starterPacks[i].mySolution = &solutions[i];
+        starterPacks[i].sps_pool = sps_pool;
     }
 
     pthread_t threads[input_data.t];
@@ -249,8 +307,10 @@ int main()
     solution_print(best_solution);
 
     branch_pool_destroy(common_branch_pool);
+    sps_pool_destroy(sps_pool);
 
-    printf("LOCKS: %d - FREES: %d", locks, frees);
+    if (locks == frees) printf("ALL GOOD!\n");
+    printf("MAX SPS USE: %d\n", max_sps_use);
     
     return 0;
 }
